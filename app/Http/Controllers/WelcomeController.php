fe401af5deef55a64d27dc599ec599ecaf962662
iannstronaut <albyanagungshafiqri@gmail.com>
2024-02-28 15:45:05 +0700

    feat: View

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -8,6 +8,11 @@ class WelcomeController extends Controller
 {
     public function index() {
         return 'Hello World';
-       }
-       
+    }
+     
+    public function greeting(){
+        return view('blog.hello')
+            ->with('name', 'Andi')
+            ->with('occupation', 'Astronaut');
+    }
 }
